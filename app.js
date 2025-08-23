50b9d43aca7ad185c04ef3a75868388c202276b1
nati <nati.floresgz@gmail.com>
2025-08-22 23:24:43 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const inputAmigo= document.getElementById("amigo");
+const listaAmigos=[];
+const ulListaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado");
+
+function agregarAmigo(){
+    
+    listaAmigos.push(inputAmigo.value);
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`; 
+};
+
+function sortearAmigo(){
+    const random = Math.floor(Math.random() * listaAmigos.length);
+    console.log(random);
+    const amigoSecreto = listaAmigos[random];
+    ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
+
+};
